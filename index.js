f711803fb97429ec371059797619a63c939b09e1
lim-alamil <limuellealamil328@gmail.com>
2022-05-20 21:38:32 +0800

    Added an owl-carousel using jQuery.

--- index.js
+++ index.js
@@ -25,3 +25,25 @@ addEventListener("load", function() {
     var viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
 })
+
+$(".carousel").owlCarousel({
+    margin: 10,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000, // 2000 ms = 2 s
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        }
+    }
+});
