8f906df1dcb6dcaf162c0106d7d4f32d7e4fcf39
lim-alamil <limuellealamil328@gmail.com>
2022-05-22 16:06:49 +0800

    Code cleanup. Added documentation (comments) in the code.

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
-
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
-})
-
 // NAVBAR SCROLL ANIMATION
+// Creates a smooth transition in the appearance/disappearance
+// of the navigation bar when scrolling.
 window.addEventListener('scroll', function () {
     let navbar = document.querySelector('nav');
     let windowPosition = window.scrollY > 0;
     navbar.classList.toggle('scrolling-active', windowPosition);
 })
 
-// SIDE MENU OF THE NAVBAR
+
+// SIDE MENU
+// Shows the navigation links at the right side of the screen
+// when the hamburger menu is clicked.
 var menu = document.getElementsByClassName('menu') [0]
 var toggle = document.getElementsByClassName('toggle-btn') [0]
 var close = document.getElementsByClassName('close-btn') [0]
@@ -38,22 +27,20 @@ if(close) {
     })
 }
 
-// SMOOTH SCROLL
-$('.link').click(function(){
-    $('html').css("scrollBehavior", "smooth");
-});
 
-// makes the screen not shrink when a keyboard appears
+// Makes the screen not shrink when a keyboard appears.
 addEventListener("load", function() {
     var viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
 })
 
+
+// Code for the carousel using jQuery.
 $(".carousel").owlCarousel({
     margin: 10,
     loop: true,
     autoplay: true,
-    autoplayTimeout: 2000, // 2000 ms = 2 s
+    autoplayTimeout: 2000, // 2000ms == 2s
     autoplayHoverPause: true,
     responsive: {
         0: {
